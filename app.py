dc20f914cd8a16df2def6c7ae5ffc0ea826b3059
Tara <tara.petric@gmail.com>
2019-04-02 17:59:36 +0200

    dodati svi periodi

--- app.py
+++ app.py
@@ -3,26 +3,45 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import numpy as np
-
-df = pd.read_csv('00000DAD_KI_KI_Sljiva_2016-11-01 00:00:00.csv',usecols=[0,1],index_col="Date",names=["Date","Tavg"])
+import os
+from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
+csv_fnames = [d for d in os.listdir(".") if d.startswith("00000")]
 
-app.layout = html.Div (children = [html.H1("PIS podaci"),
-                                   dcc.Graph(id="example",
-                                                 figure= {
-                                                     'data': [
-                                                         {'x' : df.index,'y':df.Tavg, 'type': 'line','name':'Tavg'}
-                                                         ],
-                                                      'layout': {
-                                                          'title':"Average temperature"
+graph_list = []
+for fname in csv_fnames:
+    print (fname)
+#    df = pd.read_csv(fname,names=["Date","Tavg","Tmax","Tmin","Havg","Hmin","Hmax","prec","opstina","mesto","usev"],index_col="Date")[["Tavg","opstina","mesto","usev"]]
+    df = pd.read_csv(fname,usecols=[0,1],index_col="Date",names=["Date","Tavg"])
+    graph_list.append(dcc.Graph(id=fname,
+              figure= {
+                  'data': [
+                      {'x' : df.index,'y':df.Tavg, 'type': 'line','name':'Tavg'}
+                  ],
+                  'layout': {
+                      'title':"Average temperature {}".format(fname.split("_")[0:4])
+                  }
+              })
+    )
 
-                                                          }
-                                                     })
-                                                          ])
+    
+childrenn = [html.H1("PIS podaci"),
+                                   dcc.Input (id="input", value = "", type="text"),
+                                   html.Div(id="graphs",children=graph_list)]
+
+app.layout = html.Div (children=childrenn)
+
+
+
+#@app.callback(
+#    Output(component_id="output", component_property="children"),
+#    [Input(component_id='input', component_property="value")])
+#def update_value(input_data):
+#    return "Input : {}".format(input_data)
                                    
 if __name__ == "__main__":
     app.run_server(debug=True)
