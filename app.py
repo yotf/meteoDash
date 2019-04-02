cfacca3afb96ed4562ae5ea8e2294ce0c808470c
Tara <tara.petric@gmail.com>
2019-04-02 11:54:31 +0200

    Added files

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
-import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import numpy as np
 
+df = pd.read_csv('00000DAD_KI_KI_Sljiva_2016-11-01 00:00:00.csv',usecols=[0,1],index_col="Date",names=["Date","Tavg"])
 
-app = dash.Dash(__name__)
-server = app.server
+app = dash.Dash()
 
+app.layout = html.Div (children = [html.H1("Hello world!"),
+                                   dcc.Graph(id="example",
+                                                 figure= {
+                                                     'data': [
+                                                         {'x' : df.index,'y':df.Tavg, 'type': 'line','name':'boats'}
+                                                         ],
+                                                      'layout': {
+                                                          'title':"Basic Dash Example"
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
-
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-
-
-@app.callback(dash.dependencies.Output('display-value', 'children'), [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-
-
-if __name__ == '__main__':
+                                                          }
+                                                     })
+                                                          ])
+                                   
+if __name__ == "__main__":
     app.run_server(debug=True)
+                                   
