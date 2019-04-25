528e7130470bd02f15408dd265ee87c9594435ea
Tara <tara.petric@gmail.com>
2019-04-25 23:24:38 +0200

    dodato za biranje vrednosti i dnevnih

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import dash_html_components as html
 import pandas as pd
 import numpy as np
 import os
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from collections import defaultdict
 
 from plotly import tools
 import plotly.plotly as py
 import plotly.graph_objs as go
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -22,18 +21,49 @@ graphdict = dict()
 
 sorte = list(set([x.split("_")[3] for x in csv_fnames]))
 
-
+sorte_traduction = {"Kruska":"Pear","Jabuka":"Apple","Jabuka Ogled":"Apple Experiment","Vinova Loza":"Grape Vine","Sljiva":"Plum"}
 dropdown= dcc.Dropdown(id = "sorte_dropdown",
-                       options=[{'label':sorta,'value':sorta} for sorta in sorte],
+                       options=[{'label':sorte_traduction[sorta],'value':sorta} for sorta in sorte],
                        value=["Kruska"],
                        multi =True
                        
     
     )
 
+radio_ds = dcc.RadioItems(
+    options=[
+            {'label': 'Average Daily', 'value': 'daily'},
+            {'label': 'Hourly', 'value': 'hourly'}
+
+        ],
+    value='hourly',
+    id ="koje"
+    )
+
+dropdown_funkc = dcc.Dropdown(id="panel_dropdown",
+                              options = [{'label':"Pregled satnih vrednosti",'value':"Tavg"},
+                                         {'label':"Fenologija",'value':"fen"}]
+                              )
+
+#{'label':"Bowen daily", "value" :"bowen"},
+#{'label':"Temperature Tendency daily", "value" :"tendt"},
+#{'label':"Q Tendency daily", "value" :"tendq"}
+
+
+dropdown_vrednosti = dcc.Dropdown(id="vrednosti_drop",
+                                  options = [{'label':"Average temperature",'value':"Tavg"},
+                                             {'label':"Relative humidity", "value" : "RH"},
+                                             {'label':"LCL", "value" :"lcl"},
+                                             {'label':"q", "value" :"q"},
+                                             {'label':"qsat", "value" :"qsat"},
+                                             {'label':"Dew Point", "value" :"dewpoint"}
+                                             ],
+                                  value="Tavg"
+                                  )
+
     
 childrenn = [html.H1("PIS podaci"),
-                                   dropdown,
+                                   radio_ds,dropdown_vrednosti,dropdown,
                                    html.Div(id="graphs")]
 
 app.layout = html.Div (children=childrenn)
@@ -41,31 +71,28 @@ app.layout = html.Div (children=childrenn)
 
 @app.callback(
     Output(component_id='graphs', component_property='children'),
-    [Input(component_id='sorte_dropdown', component_property='value')]
+    [Input(component_id='sorte_dropdown', component_property='value'),Input(component_id='vrednosti_drop',component_property='value'),Input(component_id="koje",component_property="value")]
 )
-def update_output_div(sorte_list):
+def update_output_div(sorte_list,vrednost,koje):
     min_date = None
     max_date = None
     graphlist = []
     for sorta in sorte_list:
         graphlist_sorta=[]
-        if sorta in graphdict.keys(): #ako smo je vec ucitali, samo ih spojimo i continue
-            graphlist = graphlist + graphdict[sorta]
-            continue
         print (sorta)
         po_sorti = [fname for fname in csv_fnames if sorta in fname]
         dataframes = dict()
         for fname in po_sorti:
-            df = pd.read_csv(fname,usecols=[0,1],index_col="Date",converters={"Date":pd.to_datetime})
-            df.columns = ["Tavg"]
+            df = pd.read_csv(fname,index_col="Date",converters={"Date":pd.to_datetime})
             min_date = df.index[0] if not min_date else (df.index[0] if df.index[0] <min_date else min_date)
             max_date = df.index[-1] if not max_date else (df.index[-1] if df.index[-1] > max_date else max_date)
             dataframes[fname] =df
         for fname,df in dataframes.items():
+            df = df if koje=="hourly" else df.resample('D').mean()
             graphlist_sorta.append(dcc.Graph(id=fname,
               figure= {
                   'data': [
-                      {'x' : df.index,'y':df.Tavg, 'type': 'line','name':'Tavg'}
+                      {'x' : df.index,'y':df[vrednost], 'type': 'line','name':vrednost}
                   ],
                   'layout': {
                       'title':"Average temperature {}".format(fname.split("_")[0:4]),
